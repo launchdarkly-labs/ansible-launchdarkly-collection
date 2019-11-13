2c01bcb75d383169848f40ddf1f2a04aeb5fc802
Dan O'Brien <dobrien@launchdarkly.com>
2019-11-13 07:21:33 -0500

    docs: gh-pages generator

---
+++ scripts/sphinx-gh-pages.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# https://github.com/lotharschulz/sphinx-pages
+set -euo pipefail
+IFS=$'\n\t'
+
+buildDirectory=_build
+
+# get a clean master branch assuming
+git checkout master
+git pull origin master
+git clean -df
+git checkout -- .
+git fetch --all
+
+# build html docs from sphinx files
+sphinx-build -b html docs/source "$buildDirectory"
+
+# create or use orphaned gh-pages branch
+branch_name=gh-pages
+if [ "$(git branch --list "$branch_name")" ]
+then
+	git stash
+	git checkout $branch_name
+	git pull origin $branch_name
+	git stash apply
+	git checkout stash -- . # force git stash to overwrite added files
+else
+	git checkout --orphan "$branch_name"
+fi
+
+if [ -d "$buildDirectory" ]
+then
+    for f in ./*
+    do
+    case $f in
+        *_build) true;;
+        *) ls "$f";;
+    esac
+    done
+	mv "${buildDirectory}"/* . && rm -rf "${buildDirectory}"
+	git add .
+	git commit -m "new pages version $(date)"
+	git push origin gh-pages
+else
+	echo "directory $buildDirectory does not exists"
+fi
