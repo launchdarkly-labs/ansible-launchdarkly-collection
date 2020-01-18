b3c195e50e15ff9ce08a3da678be3ce2f38ac839
Dan O'Brien <dobrien@launchdarkly.com>
2020-01-18 10:12:06 -0500

    testing

--- tests/integration/targets/launchdarkly_custom_role/runme.sh
+++ tests/integration/targets/launchdarkly_custom_role/runme.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# shellcheck source=tests/runme_base.sh
-source "$(git rev-parse --show-toplevel)"/tests/runme_base.sh
+
+if [[ -z "$LAUNCHDARKLY_ACCESS_TOKEN" ]] &&
+   [[ -z "$LAUNCHDARKLY_DEST_ACCESS_TOKEN" ]] &&
+   [[ -z "$LAUNCHDARKLY_SDK_KEY" ]]
+then
+    :
+else
+    # shellcheck source=tests/runme_base.sh
+    source "$(git rev-parse --show-toplevel)"/tests/runme_base.sh
+fi
+
+
+
 
 ansible-playbook -vvvv test_custom_role.yml
