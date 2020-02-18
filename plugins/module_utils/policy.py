a1dda7a63cfe470060723b4ea9ce1b59ac2c0209
Dan O'Brien <dobrien@launchdarkly.com>
2020-02-18 12:02:49 -0500

    core: adding not resources and actions

--- plugins/module_utils/policy.py
+++ plugins/module_utils/policy.py
@@ -3,9 +3,13 @@ def policy_argument_spec():
         type="list",
         elements="dict",
         aliases=["statement", "statements", "policies"],
+        required_one_of=[["resources", "not_resources"], ["actions", "not_actions"]],
+        mutually_exclusive=[["resources", "not_resources"], ["actions", "not_actions"]],
         options=dict(
             resources=dict(type="list", elements="str"),
-            actions=dict(type="list", elements="str", default="*"),
+            not_resources=dict(type="list", elements="str"),
+            actions=dict(type="list", elements="str"),
+            not_actions=dict(type="list", elements="str"),
             effect=dict(type="str", choices=["allow", "deny"]),
         ),
     )
