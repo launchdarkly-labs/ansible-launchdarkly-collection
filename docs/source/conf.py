85b06eae8127b3d7bd8ad91a5df60806e411d92a
Dan O'Brien <dobrien.nj@gmail.com>
2020-03-08 20:20:30 -0400

    meta: fix rate limit again (#10)
    Co-authored-by: Dan O'Brien <dobrien@launchdarkly.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@
 project = "LaunchDarkly Ansible Collection"
 copyright = "2019, Catamorphic, Co."
 author = "Dan O'Brien"
-version = "0.2.9"
+version = "0.2.10"
 
 
 # -- General configuration ---------------------------------------------------
@@ -60,7 +60,7 @@ html_context = {
     "github_version": "tree/master/plugins/",
     "github_module_version": "devel/lib/ansible/modules/",
     "current_version": version,
-    "latest_version": "0.2.9",
+    "latest_version": "0.2.10",
     # list specifically out of order to make latest work
-    "available_versions": ("latest", "0.2.9"),
+    "available_versions": ("latest", "0.2.10"),
 }
