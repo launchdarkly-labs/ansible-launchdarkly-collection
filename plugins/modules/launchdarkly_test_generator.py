d280db60be47506251f746da3fd85f5a4df8c31b
Dan O'Brien <dobrien@launchdarkly.com>
2020-03-14 18:25:16 -0400

    core: fix test generator bug

--- plugins/modules/launchdarkly_test_generator.py
+++ plugins/modules/launchdarkly_test_generator.py
@@ -107,16 +107,17 @@ def main():
     )
 
     test_data = json.loads(resp.read())
-    for item in module.params["overrides_flag"]:
-        if item.values()[0] not in test_data["flags"][item.keys()[0]]["variations"]:
-            raise AnsibleError("Override variation does not match flag variations")
+    if module.params.get("overrides_flag"):
+        for item in module.params["overrides_flag"]:
+            if item.values()[0] not in test_data["flags"][item.keys()[0]]["variations"]:
+                raise AnsibleError("Override variation does not match flag variations")
 
-        if "flagValues" in test_data.keys():
-            test_data["flagValues"][item.keys()[0]] = item.values()[0]
-        else:
-            test_data["flagValues"] = {}
-            test_data["flagValues"][item.keys()[0]] = item.values()[0]
-        del test_data["flags"][item.keys()[0]]
+            if "flagValues" in test_data.keys():
+                test_data["flagValues"][item.keys()[0]] = item.values()[0]
+            else:
+                test_data["flagValues"] = {}
+                test_data["flagValues"][item.keys()[0]] = item.values()[0]
+            del test_data["flags"][item.keys()[0]]
 
     module.exit_json(changed=True, content=test_data)
 
