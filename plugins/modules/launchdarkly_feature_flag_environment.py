852d6b007261d26cb17b78d68b780440b01dfefd
Dan O'Brien <dobrien@launchdarkly.com>
2020-06-05 16:52:57 -0400

    lint

--- plugins/modules/launchdarkly_feature_flag_environment.py
+++ plugins/modules/launchdarkly_feature_flag_environment.py
@@ -511,9 +511,12 @@ def _process_rules(module, patches, feature_flag):
                             )
                         )
 
-                    if rule["clauses"] is not None and (flag.get("clauses") and list(
-                        diff(rule["clauses"], flag["clauses"], ignore=set(["id"]))
-                    )):
+                    if rule["clauses"] is not None and (
+                        flag.get("clauses")
+                        and list(
+                            diff(rule["clauses"], flag["clauses"], ignore=set(["id"]))
+                        )
+                    ):
                         for clause_idx, clause in enumerate(rule["clauses"]):
                             patches.append(
                                 _patch_op(
