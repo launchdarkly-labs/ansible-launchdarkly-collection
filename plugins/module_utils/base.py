2c21f589b0a889ddf4e0ad0a04f6b8c586585abd
Dan O'Brien <dobrien.nj@gmail.com>
2020-06-05 08:17:11 -0400

    better 429 handling (#25)
    * better 429 handling
    
    * add makefile help
    
    * update version

--- plugins/module_utils/base.py
+++ plugins/module_utils/base.py
@@ -5,7 +5,7 @@ from ansible.errors import AnsibleError, AnsibleAuthenticationFailure
 from ansible.module_utils.common._json_compat import json
 from ansible.module_utils.basic import env_fallback
 
-VERSION = "0.3.3"
+VERSION = "0.3.4"
 
 
 def configure_instance(api_key):
