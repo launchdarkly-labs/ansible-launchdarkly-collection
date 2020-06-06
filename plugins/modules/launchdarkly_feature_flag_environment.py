0c141a2db6b9ba431c5beb202d63dffa573b70cc
Dan O'Brien <dobrien@launchdarkly.com>
2020-06-05 21:09:41 -0400

    fix var

--- plugins/modules/launchdarkly_feature_flag_environment.py
+++ plugins/modules/launchdarkly_feature_flag_environment.py
@@ -421,7 +421,7 @@ def _configure_feature_flag_env(module, api_instance, feature_flag=None):
             msg="flag environment successfully configured",
             feature_flag_environment=api_response.to_dict(),
             patches=output_patches,
-            clauses=clauses,
+            clauses=clause_list,
         )
 
     module.exit_json(
