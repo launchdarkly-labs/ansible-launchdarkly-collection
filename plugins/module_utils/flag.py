917854bd5afd9caf4958f6d2b16b9bbcd724e8ec
Dan O'Brien <dobrien@launchdarkly.com>
2020-09-03 09:47:51 -0400

    fix import

--- plugins/module_utils/flag.py
+++ plugins/module_utils/flag.py
@@ -1,3 +1,16 @@
+import traceback
+
+try:
+    import launchdarkly_api
+    from launchdarkly_api.rest import ApiException
+
+    # from dictdiffer import diff
+
+    HAS_LD = True
+except ImportError:
+    LD_IMP_ERR = traceback.format_exc()
+    HAS_LD = False
+
 def defaults_configure(fflag_body, flag):
     if flag["defaults"] and flag["defaults"] is not None:
         fflag_body["defaults"] = dict(
